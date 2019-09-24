15f807f103ecf576010241714b066047204052f9
weiyang <weiyang6539@gmail.com>
2019-09-24 16:28:19 +0300

    处理点击事件及图标显示

--- app/src/main/java/com/wyang/myapplication/MainActivity.java
+++ app/src/main/java/com/wyang/myapplication/MainActivity.java
@@ -1,27 +1,64 @@
 package com.wyang.myapplication;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
 import com.wyang.mylibrary.TaskLayout;
 
 /**
  * Created by weiyang on 2019-09-24.
  */
 public class MainActivity extends AppCompatActivity {
+    private TaskLayout mTaskLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TaskLayout mTaskLayout = findViewById(R.id.mTaskLayout);
+        mTaskLayout = findViewById(R.id.mTaskLayout);
+        mTaskLayout.setOnStageListener(new TaskLayout.OnStageListener() {
+            @Override
+            public void setIcon(ImageView iv, int state) {
+                if (state == TaskLayout.NORMAL) {
+                    iv.setImageResource(R.drawable.icon_normal);
+                } else if (state == TaskLayout.ACTIVE) {
+                    Glide.with(MainActivity.this)
+                            .load(R.drawable.icon_active)
+                            .into(iv);
+                } else if (state == TaskLayout.RECEIVED) {
+                    iv.setImageResource(R.drawable.icon_received);
+                }
+            }
+
+            @Override
+            public void onClick(final int stage) {
+                //调用接口领取
+                Toast.makeText(MainActivity.this, "领取中..", Toast.LENGTH_SHORT).show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "领取成功!", Toast.LENGTH_SHORT).show();
+                        mTaskLayout.setReceived(stage);
+                        mTaskLayout.setProgress(stage + 3);
+                    }
+                }, 3000);
+            }
+        });
 
         mTaskLayout.setProgress(3);
-        mTaskLayout.setMax(10);
+        mTaskLayout.setMax(24);
 
-
-        mTaskLayout.addStage(3, 88);
-        mTaskLayout.addStage(6, 188);
-        mTaskLayout.addStage(8, 888);
+        mTaskLayout.addStage(3, 88, false);
+        mTaskLayout.addStage(6, 188, false);
+        mTaskLayout.addStage(9, 388, false);
+        mTaskLayout.addStage(12, 588, false);
+        mTaskLayout.addStage(15, 588, false);
+        mTaskLayout.addStage(18, 588, false);
+        mTaskLayout.addStage(21, 588, false);
     }
 }
--- mylibrary/src/main/java/com/wyang/mylibrary/TaskLayout.java
+++ mylibrary/src/main/java/com/wyang/mylibrary/TaskLayout.java
@@ -1,10 +1,14 @@
 package com.wyang.mylibrary;
 
 import android.content.Context;
+import android.support.annotation.IntDef;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.util.SparseArray;
+import android.util.SparseBooleanArray;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Space;
@@ -35,6 +39,8 @@ public class TaskLayout extends ConstraintLayout {
         mProgressBar = findViewById(R.id.mProgressBar);
 
         LayoutParams params = (LayoutParams) mProgressBar.getLayoutParams();
+        params.leftToLeft = getId();
+        params.rightToRight = getId();
         params.bottomToBottom = getId();
         params.bottomMargin = dp2px(37);
     }
@@ -45,14 +51,35 @@ public class TaskLayout extends ConstraintLayout {
 
     public void setProgress(int progress) {
         mProgressBar.setProgress(progress);
+        for (int i = 0; i < iconArr.size(); i++) {
+            int stage = iconArr.keyAt(i);
+            ImageView icon = iconArr.valueAt(i);
+            int state = states.get(stage);
+
+            if (stage > progress)
+                continue;
+
+            if (state == RECEIVED)
+                continue;
+
+            states.put(stage, ACTIVE);
+
+            if (onStageListener != null) {
+                onStageListener.setIcon(icon, ACTIVE);
+            }
+        }
     }
 
     private SparseArray<Space> baseLine = new SparseArray<>();
     private SparseArray<ImageView> iconArr = new SparseArray<>();
     private SparseArray<TextView> moneyArr = new SparseArray<>();
     private SparseArray<TextView> stageArr = new SparseArray<>();
+    //状态记录
+    private SparseIntArray states = new SparseIntArray();
 
-    public void addStage(int stage, int money) {
+    public void addStage(final int stage, int money, boolean isReceive) {
+        states.put(stage, isReceive ? RECEIVED : NORMAL);
+
         float per = stage * 1.0f / mProgressBar.getMax();
 
         Space space = new Space(getContext());
@@ -63,11 +90,35 @@ public class TaskLayout extends ConstraintLayout {
         params.leftToLeft = getId();
         params.rightToRight = getId();
         params.horizontalBias = per;
+        space.setLayoutParams(params);
 
         //初始化icon
         ImageView icon = new ImageView(getContext());
         icon.setAdjustViewBounds(true);
-        icon.setImageResource(R.drawable.icon_normal);
+        icon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int state = states.get(stage);
+                //已领取或未到领取状态
+                if (state == RECEIVED || stage > mProgressBar.getProgress())
+                    return;
+
+                if (onStageListener != null)
+                    onStageListener.onClick(stage);
+            }
+        });
+        if (onStageListener != null) {
+            if (stage > mProgressBar.getProgress()) {
+                //未到领取状态
+                onStageListener.setIcon(icon, NORMAL);
+            } else if (!isReceive) {
+                //领取状态
+                onStageListener.setIcon(icon, ACTIVE);
+            } else {
+                //已领取状态不显示gif
+                onStageListener.setIcon(icon, RECEIVED);
+            }
+        }
         addView(icon);
         iconArr.append(stage, icon);
 
@@ -78,6 +129,7 @@ public class TaskLayout extends ConstraintLayout {
         params1.rightToRight = space.getId();
         params1.bottomToBottom = mProgressBar.getId();
         params1.bottomMargin = dp2px(24);
+        icon.setLayoutParams(params1);
 
         //初始化金额
         TextView tvMoney = new TextView(getContext());
@@ -92,6 +144,7 @@ public class TaskLayout extends ConstraintLayout {
         params2.rightToRight = space.getId();
         params2.bottomToBottom = mProgressBar.getId();
         params2.bottomMargin = dp2px(7);
+        tvMoney.setLayoutParams(params2);
 
         //初始化阶段
         TextView tvStage = new TextView(getContext());
@@ -106,6 +159,7 @@ public class TaskLayout extends ConstraintLayout {
         params3.rightToRight = space.getId();
         params3.topToTop = mProgressBar.getId();
         params3.topMargin = dp2px(12);
+        tvStage.setLayoutParams(params3);
 
     }
 
@@ -124,6 +178,11 @@ public class TaskLayout extends ConstraintLayout {
             removeView(tvStage);
     }
 
+    public void setReceived(int stage) {
+        states.put(stage, RECEIVED);
+        if (onStageListener != null)
+            onStageListener.setIcon(iconArr.get(stage), RECEIVED);
+    }
 
     private int dp2px(float dp) {
         final float scale = getContext().getResources().getDisplayMetrics().density;
@@ -133,5 +192,25 @@ public class TaskLayout extends ConstraintLayout {
     private int px2dp(float px) {
         final float scale = getContext().getResources().getDisplayMetrics().density;
         return (int) (px / scale + 0.5f);
+    }
+
+    private OnStageListener onStageListener;
+
+    public void setOnStageListener(OnStageListener onStageListener) {
+        this.onStageListener = onStageListener;
+    }
+
+    public interface OnStageListener {
+        void setIcon(ImageView iv, @State int state);
+
+        void onClick(int stage);
+    }
+
+    public final static int NORMAL = 1;
+    public final static int ACTIVE = 2;
+    public final static int RECEIVED = 3;
+
+    @IntDef({NORMAL, ACTIVE, RECEIVED})
+    public @interface State {
     }
 }
